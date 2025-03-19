9ed732a51e8d587984e83db4432182d600d9e5fa
Riccardo Orlandi <riccardo.orlandi0803@icloud.com>
2025-03-19 13:01:28 +0100

    aggiornati i file .ino epunto4 e power_estimation

--- project1/power_estimation.py
+++ project1/power_estimation.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-
 
 
 Y = 8402 % 5000 + 15000 #1071(8402)
 
-# Carica i file CSV
-df1 = pd.read_csv(os.path.join(os.getcwd(), 'project1', 'data', 'deep_sleep.csv'))
-df2 = pd.read_csv(os.path.join(os.getcwd(), 'project1', 'data', 'sensor_read.csv'))
-df3 = pd.read_csv(os.path.join(os.getcwd(), 'project1', 'data', 'transmission_power.csv'))
+# Load the CSV file
+df1 = pd.read_csv('/Users/riccardoorlandi/Desktop/universita/5anno/2semestre/INTERNET_OF_THINGS/projects/IoT_project/project1/data/deep_sleep.csv')
+df2 = pd.read_csv('/Users/riccardoorlandi/Desktop/universita/5anno/2semestre/INTERNET_OF_THINGS/projects/IoT_project/project1/data/sensor_read.csv')
+df3 = pd.read_csv('/Users/riccardoorlandi/Desktop/universita/5anno/2semestre/INTERNET_OF_THINGS/projects/IoT_project/project1/data/transmission_power.csv')
 
 #extract the data column
 data_values1 = df1['Data']
@@ -24,6 +22,7 @@ wifi_on = data_values1[(data_values1 > 700) & (data_values1 < 800)]
 sensor_reading =data_values2[data_values2 > 460]
 idle_sensor =data_values2[data_values2 < 340]
 deep_sleep = data_values1[data_values1 < 100]
+transmission = data_values3[data_values3 > 1200] #19.5dbm
 
 avg_power_boot = boot.mean()
 avg_power_wifi_off = wifi_off.mean() #IDLE
@@ -31,12 +30,12 @@ avg_power_wifi_on = wifi_on.mean()
 avg_power_sensor_reading = sensor_reading.mean()
 avg_power_idle_sensor = idle_sensor.mean()
 avg_power_deep_sleep = deep_sleep.mean()
-
+avg_power_transmission = transmission.mean()
 
 # Define the duration of each state (in seconds)
 duration_boot =0.000550 #[s]
-duration_wifi_off = 0.182500 #[s]
-duration_wifi_on = 0.195000
+duration_wifi_off = 0.201000 #[s]
+duration_wifi_on = 0.001000 #[s]
 duration_transmission = 0.000150 #[s] wifi.on non cosidero la distanza
 duration_deep_sleep = 7.0 #[s]
 duration_sensor_reading = 0.018300 #[s]
@@ -51,31 +50,32 @@ duty_cycle_sensor = (duration_sensor_reading/duration_idle_sensor)*100
 
 
 # Calculate the energy consumption for each state (in mJ)
-energy_boot = (avg_power_boot+avg_power_idle_sensor)*duration_boot*1000
-energy_wifi_off = (avg_power_wifi_off+avg_power_idle_sensor)*duration_wifi_off*1000
-energy_wifi_on = (avg_power_wifi_on+avg_power_sensor_reading)*duration_sensor_reading*1000 +(avg_power_wifi_on+avg_power_idle_sensor)*(duration_wifi_on-duration_sensor_reading)*1000
+energy_boot = (avg_power_boot + avg_power_idle_sensor)*duration_boot*1000
+energy_wifi_off = (avg_power_wifi_off + avg_power_idle_sensor)*(duration_wifi_off - duration_sensor_reading)*1000+(avg_power_sensor_reading)*duration_sensor_reading*1000
+energy_wifi_on = (avg_power_wifi_on+avg_power_idle_sensor)*(duration_wifi_on-duration_transmission)*1000 +(avg_power_transmission+avg_power_idle_sensor)*(duration_transmission)*1000
 energy_deep_sleep = (avg_power_deep_sleep+avg_power_idle_sensor) * duration_deep_sleep * 1000  
 
 energy_sensor_reading = avg_power_sensor_reading*duration_sensor_reading*1000
-energy_transmission = avg_power_wifi_on*duration_transmission
+energy_transmission = avg_power_transmission*duration_transmission*1000
 
 # Calculate the total energy consumption for one transmission cycle (in mJ)
-total_energy_consumption = (energy_boot+energy_wifi_off+energy_wifi_on+energy_deep_sleep)/1000 #[J]
+total_energy_consumption = (energy_boot+energy_wifi_off+energy_wifi_on+energy_deep_sleep)/1000 #[mJ]
 
 # Print the results
 print(f"Average Power Consumption during Boot: {avg_power_boot} mW")
 print(f"Average Power Consumption during Idle: {avg_power_wifi_off} mW")
 print(f"Average Power Consumption during Transmission: {avg_power_wifi_on} mW")
 print(f"Average Power Consumption during Deep Sleep: {avg_power_deep_sleep} mW")
-print(f"Total Energy Consumption for one Transmission Cycle: {total_energy_consumption} J")
+print(f"Total Energy Consumption for one Transmission Cycle: {total_energy_consumption} mJ")
 
 
-######################## POINT 3:
+######################## POINT 2.2(battery estimation):
 #BATTERY_ENERGY:
-Y = 18402 #1071(8402) [mJ]
-number_cycle = Y/total_energy_consumption
+Y = 18402 #1071(8402) [J]
+number_cycle = (Y*1000)/total_energy_consumption
 print(number_cycle)
-
+total_time = number_cycle*duration_tot
+print(total_time)
 ###########################
 
 # Dati calcolati
@@ -107,6 +107,8 @@ for i, label in enumerate(labels):
 
 plt.show()
 
+
+
 ###############################################################
 #Starting from the system requirements, propose some possible
 #Improvements aiming to reduce the Energy Consumption without
@@ -114,9 +116,10 @@ plt.show()
 #node the occupancy state of a parking spot»
 
 #1.dato che si tratta di un parcheggio si potrebbe modificare la potenza di trasmissione del segnale da 19.5dbm a valori inferiori 
-#2.il sensore rimane sempre alimentato e quindi consuma energia costanetemente, si potrebbe agigungere un interruttore in modo che si accedna solo quando wifi è on
+#2.evitare idle:(switch to low 3v3 pin)il sensore rimane sempre alimentato e quindi consuma energia costanetemente, si potrebbe agigungere un interruttore in modo che si accedna solo quando wifi è on
 #3.dato che il sensore produce un output binario, l'invio del messaggio potrebbe essere mandato solo in uno dei due casi e inviare ogni tot un messaggio che indica che la trasmissione è comunque on
-#4. dato che si tratta di un parcheggio, avere come nel nostro caso un duty cycle di 7 secondi risulta troppo eccessivo e si potrebbe aumentare il deepsleep a qualche minuto.
-#5. ridurre la potenza del sengale di trasmissione da 19.5dbm a 10dbm circa per poter essere in grado di raggiungere il segnale piu lontano ma evitando di sprecare energia 
+#3.1 messaggio solo quando l'output cambia il suo stato.
+#4.dato che si tratta di un parcheggio, avere come nel nostro caso un duty cycle di 7 secondi risulta troppo eccessivo e si potrebbe aumentare il deepsleep a qualche minuto.
+#5.ridurre la potenza del sengale di trasmissione da 19.5dbm a 10dbm circa per poter essere in grado di raggiungere il segnale piu lontano ma evitando di sprecare energia.
 #6.considerare di utilizzare la modalita light sleep 
 

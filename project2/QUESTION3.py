a4f946d34543722fd9fe358cd2a746c60cf946dc
RiccardoOrlandi <166627174+RiccardoOrlandi@users.noreply.github.com>
2025-04-02 15:21:54 +0200

    Update QUESTION3.py

--- project2/QUESTION3.py
+++ project2/QUESTION3.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import re
-
+#(ip.dst == 35.158.43.69 or ip.dst == 35.158.34.213 or ip.dst == 18.192.151.104) and mqtt and mqtt.topic contains "#"
 # Percorsi dei file CSV
 file_path = "pacchetti_filtrati/question3/clients_with_multilevel_wildcards_hivemq.csv"
 df = pd.read_csv(file_path)
